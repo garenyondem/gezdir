4e49b023b31a05773f0dfc73d9177feea20e523a
Garen Yöndem <garen@yondem.com>
2017-03-25 00:05:58 +0300

    Added constants file

---
+++ Gezdir.Service/Gezdir/resources/constants.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+const collectionNames = {
+    Users: 'Users',
+    UserLocations: 'UserLocations',
+    Events: 'Events',
+    EventTypes: 'EventTypes'
+}
+
+module.exports = {
+    collectionNames: collectionNames
+}
