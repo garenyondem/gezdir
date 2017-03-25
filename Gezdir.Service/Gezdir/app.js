bf7a8f577445a323cec42ded179aec917e3256af
Garen Yöndem <garen@yondem.com>
2017-03-26 01:55:29 +0300

    Added tickets endpoint

--- Gezdir.Service/Gezdir/app.js
+++ Gezdir.Service/Gezdir/app.js
@@ -5,10 +5,12 @@ var express = require('express'),
     db = require('./db/db'),
     UserController = require('./controllers/user'),
     EventController = require('./controllers/event'),
-    EventTypeController = require('./controllers/eventType');
+    EventTypeController = require('./controllers/eventType'),
+    TicketController = require('./controllers/ticket');
 
 app.use('/user', UserController);
 app.use('/events', EventController);
 app.use('/eventTypes', EventTypeController);
+app.use('/tickets', TicketController);
 
 module.exports = app;
